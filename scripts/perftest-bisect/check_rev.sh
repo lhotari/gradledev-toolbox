ef9adf9d64e379844cebe5cdcb40fac80d396029
Lari Hotari <lari@hotari.net>
2016-06-17 14:15:00 +0300

    update script

--- scripts/perftest-bisect/check_rev.sh
+++ scripts/perftest-bisect/check_rev.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-./gradlew clean
-cp -Rdvp ~/.gradle-bisect-override/* .
+# script to be used for Gradle performance test bisecting
+# example usage:
+# git bisect start HEAD REL_2.14 --  # HEAD=bad REL_2.14=good
+# git bisect run check_rev.sh JavaConfigurationPerformanceTest lotDependencies
 TESTNAME=${1:-IdeIntegrationPerformanceTest}
 TESTPROJECT=${2:-multi}
-./gradlew -S -x :performance:prepareSamples :performance:$TESTPROJECT :performance:cleanPerformanceTest :performance:performanceTest -D:performance:performanceTest.single=$TESTNAME
+./gradlew clean
+[ -d ~/.gradle-bisect-override ] && cp -Rdvp ~/.gradle-bisect-override/* .
+./gradlew -S -PtimestampedVersion -x :performance:prepareSamples :performance:$TESTPROJECT :performance:cleanPerformanceTest :performance:performanceTest -D:performance:performanceTest.single=$TESTNAME
 result=$?
 hash=$(git rev-parse HEAD | colrm 9)
 datets=$(date +%Y-%m-%d-%H:%M:%S)
