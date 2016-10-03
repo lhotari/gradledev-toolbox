1c79098b9e8d2cb33d5c467022c5ead9a01942e8
Lari Hotari <lari@hotari.net>
2016-10-03 13:01:35 +0300

    Use pid in gradle.pid file as pid of profiling target

--- functions/gradledev-perftest-functions.sh
+++ functions/gradledev-perftest-functions.sh
@@ -429,13 +429,21 @@ function gradledev_honestprofiler_flamegraph {
     )
 }
 
+function gradledev_profiling_target_pid {
+    if [ -f gradle.pid ]; then
+        cat gradle.pid
+    else 
+        echo `gradledev_daemon_pid`
+    fi
+}
+
 function gradledev_jfr_start {
-    DAEMON_PID=`gradledev_daemon_pid`
+    DAEMON_PID=`gradledev_profiling_target_pid`
     jcmd $DAEMON_PID JFR.start name=GradleDaemon_$DAEMON_PID settings=$GRADLEDEV_TOOLBOX_DIR/etc/jfr/profiling.jfc maxsize=1G
 }
 
 function gradledev_jfr_stop {
-    DAEMON_PID=`gradledev_daemon_pid`
+    DAEMON_PID=`gradledev_profiling_target_pid`
     FILENAME="$PWD/GradleDaemon_${DAEMON_PID}_$(date +%F-%T).jfr"
     jcmd $DAEMON_PID JFR.stop name=GradleDaemon_$DAEMON_PID filename=$FILENAME
     if [[ "$1" == "open" ]]; then
